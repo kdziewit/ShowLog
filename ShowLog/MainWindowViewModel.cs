e5d9b17a92c9ceadb84b2fb33c1b7c99dd5db6a1
Krzysztof Dziewit <k.dziewit@predictivesolutions.pl>
2020-06-15 18:23:15 +0200

    Add project files.

---
+++ ShowLog/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShowLog
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<String> LogMessages { get; set; } = new ObservableCollection<string>();
+        private string logMessage;
+
+        public string LogMessage
+        {
+            get { return logMessage; }
+            set
+            {
+                logMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ShowLog/StaticTarget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ShowLog
+{
+    class StaticTarget
+    {
+        public static ObservableCollection<string> StaticLogMessages { get; set; } = new ObservableCollection<string>();
+    }
+}
